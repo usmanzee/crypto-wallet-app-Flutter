98958e17b01b5defae26ec75bb00b5832a3449ab
Asim Jawad <asim.jawad@superiorsoft.com.pk>
2021-08-30 12:36:04 +0500

    added the bank details page and the added bank widget in the payment page

--- lib/views/p2p/p2p_user_profile/p2p_payment/p2p_payment_method_add_bank_details_page.dart
+++ lib/views/p2p/p2p_user_profile/p2p_payment/p2p_payment_method_add_bank_details_page.dart
@@ -1,10 +1,148 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class P2pPaymentMethodAddBankDetailsPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backwardsCompatibility: false,
+        backgroundColor: Get.theme.scaffoldBackgroundColor,
+        foregroundColor: Get.theme.textSelectionTheme.selectionColor,
+        centerTitle: true,
+        //todo: add the bank name here
+        title: Text(
+          'Add ABN AMRO',
+          style: TextStyle(
+            fontFamily: "Popins",
+            fontWeight: FontWeight.w500,
+            fontSize: 16.0,
+            color: Get.theme.hintColor,
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: () => Get.back(),
+          child: Icon(
+            Icons.arrow_back_ios_rounded,
+            size: 20,
+            color: Get.theme.hintColor,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 16.0,
+          left: 16.0,
+          right: 16.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _textField(
+                  hintText: 'Please enter your full name',
+                  heading: 'Full name',
+                ),
+                _textField(
+                  hintText: 'Please fill in your bank account number',
+                  heading: 'Bank account number',
+                ),
+                _textField(
+                  hintText:
+                      'details like your own address, the address of the bank, the IBAN code and SWIFTBIC code, etc.',
+                  heading:
+                      'Other payment details (for internation transfer) (Optional)',
+                  maxLines: true,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Tip: The added payment method will be shown to the buyer during the transaction to accept Fiat transfers. Please make sure the information is correct.',
+                  style: TextStyle(
+                    fontFamily: "Popins",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 11.0,
+                    color: Get.theme.hintColor.withOpacity(0.7),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Get.theme.canvasColor,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      'Confirm',
+                      style: TextStyle(
+                        fontFamily: "Popins",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Get.theme.hintColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+//todo:; add the required parameters and the controllers here
+  Widget _textField({
+    @required String heading,
+    @required String hintText,
+    bool maxLines = false,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 16.0,
+        ),
+        Text(
+          heading,
+          style: TextStyle(
+            fontFamily: "Popins",
+            fontWeight: FontWeight.w600,
+            fontSize: 14.0,
+            color: Get.theme.hintColor.withOpacity(0.8),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          decoration: BoxDecoration(
+            color: Get.theme.canvasColor,
+          ),
+          child: TextFormField(
+            maxLines: maxLines ? 5 : 1,
+            decoration: InputDecoration(
+              hintText: hintText,
+              border: InputBorder.none,
+              hintStyle: TextStyle(
+                fontFamily: "Popins",
+                fontWeight: FontWeight.w600,
+                fontSize: 14.0,
+                color: Get.theme.hintColor.withOpacity(0.5),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/views/p2p/p2p_user_profile/p2p_payment/p2p_payment_method_page.dart
+++ lib/views/p2p/p2p_user_profile/p2p_payment/p2p_payment_method_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:b4u_wallet/utils/Helpers/my_imgs.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -33,27 +35,33 @@ class P2pPaymentMethodPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    MyImgs.testPhoto,
-                    height: 200,
-                    width: 200,
-                    fit: BoxFit.fill,
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  MyImgs.testPhoto,
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.fill,
+                ),
+                Text(
+                  'No Payment Method',
+                  style: TextStyle(
+                    fontFamily: "Popins",
+                    color: Get.theme.hintColor,
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w400,
                   ),
-                  Text(
-                    'No Payment Method',
-                    style: TextStyle(
-                      fontFamily: "Popins",
-                      color: Get.theme.hintColor,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+                Text('How the widget will look like'),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: _bankWidget(
+                      bankName: 'John Doe Company',
+                      accountHolderName: 'John Doe',
+                      accountNumber: '12345678765430'),
+                ),
+              ],
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -82,6 +90,74 @@ class P2pPaymentMethodPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _bankWidget({
+    @required String bankName,
+    @required String accountHolderName,
+    @required String accountNumber,
+  }) {
+    final _random = Random();
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color:
+                        Colors.primaries[_random.nextInt(Colors.primaries.length)],
+                    borderRadius: BorderRadius.circular(1),
+                  ),
+                  child: Text(' '),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      bankName,
+                      style: TextStyle(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: Text(
+                        accountHolderName,
+                        style: TextStyle(),
+                      ),
+                    ),
+                    Text(
+                      accountNumber,
+                      style: TextStyle(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Icon(
+              Icons.edit,
+              size: 20,
+              color: Get.theme.hintColor,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Container(
+          width: double.infinity,
+          height: 0.3,
+          color: Get.theme.hintColor,
+        ),
+      ],
     );
   }
 }
